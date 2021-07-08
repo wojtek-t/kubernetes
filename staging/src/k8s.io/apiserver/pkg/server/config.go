kubernetes/apiserver

Estimate width of the request based on watchers count in P&F
Kubernetes-commit: 223f9be59778b6ec2e44fd57df523f00e246bd95

pkg/server/config.go
@@ -758,7 +758,7 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	handler = filterlatency.TrackStarted(handler, "authorization")
 
 	if c.FlowControl != nil {
-		requestWorkEstimator := flowcontrolrequest.NewWorkEstimator(c.StorageObjectCountTracker.Get)
+		requestWorkEstimator := flowcontrolrequest.NewWorkEstimator(c.StorageObjectCountTracker.Get, c.FlowControl.GetInterestedWatchCount)
 		handler = filterlatency.TrackCompleted(handler)
 		handler = genericfilters.WithPriorityAndFairness(handler, c.LongRunningFunc, c.FlowControl, requestWorkEstimator)
 		handler = filterlatency.TrackStarted(handler, "priorityandfairness")

pkg/util/flowcontrol/request/width.go
@@ -65,12 +65,17 @@ func (we *WorkEstimate) MaxSeats() int {
 // number of objects for a given resource.
 type objectCountGetterFunc func(string) (int64, error)
 
+// watchCountGetterFunc represents a function that gets the total
+// number of watchers potentially interested in a given request.
+type watchCountGetterFunc func(*apirequest.RequestInfo) int
+
 // NewWorkEstimator estimates the work that will be done by a given request,
 // if no WorkEstimatorFunc matches the given request then the default
 // work estimate of 1 seat is allocated to the request.
-func NewWorkEstimator(countFn objectCountGetterFunc) WorkEstimatorFunc {
+func NewWorkEstimator(objectCountFn objectCountGetterFunc, watchCountFn watchCountGetterFunc) WorkEstimatorFunc {
 	estimator := &workEstimator{
-		listWorkEstimator: newListWorkEstimator(countFn),
+		listWorkEstimator:     newListWorkEstimator(objectCountFn),
+		mutatingWorkEstimator: newMutatingWorkEstimator(watchCountFn),
 	}
 	return estimator.estimate
 }
@@ -87,6 +92,8 @@ func (e WorkEstimatorFunc) EstimateWork(r *http.Request) WorkEstimate {
 type workEstimator struct {
 	// listWorkEstimator estimates work for list request(s)
 	listWorkEstimator WorkEstimatorFunc
+	// mutatingWorkEstimator calculates the width of mutating request(s)
+	mutatingWorkEstimator WorkEstimatorFunc
 }
 
 func (e *workEstimator) estimate(r *http.Request) WorkEstimate {
@@ -100,6 +107,8 @@ func (e *workEstimator) estimate(r *http.Request) WorkEstimate {
 	switch requestInfo.Verb {
 	case "list":
 		return e.listWorkEstimator.EstimateWork(r)
+	case "create", "update", "patch", "delete":
+		return e.mutatingWorkEstimator.EstimateWork(r)
 	}
 
 	return WorkEstimate{InitialSeats: minimumSeats}

pkg/util/flowcontrol/request/width_test.go
@@ -20,18 +20,22 @@ import (
 	"errors"
 	"net/http"
 	"testing"
+	"time"
 
 	apirequest "k8s.io/apiserver/pkg/endpoints/request"
 )
 
 func TestWorkEstimator(t *testing.T) {
 	tests := []struct {
-		name                 string
-		requestURI           string
-		requestInfo          *apirequest.RequestInfo
-		counts               map[string]int64
-		countErr             error
-		initialSeatsExpected uint
+		name                      string
+		requestURI                string
+		requestInfo               *apirequest.RequestInfo
+		counts                    map[string]int64
+		countErr                  error
+		watchCount                int
+		initialSeatsExpected      uint
+		finalSeatsExpected        uint
+		additionalLatencyExpected time.Duration
 	}{
 		{
 			name:                 "request has no RequestInfo",
@@ -248,6 +252,132 @@ func TestWorkEstimator(t *testing.T) {
 			countErr:             errors.New("unknown error"),
 			initialSeatsExpected: maximumSeats,
 		},
+		{
+			name:       "request verb is create, no watches",
+			requestURI: "http://server/apis/foo.bar/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        0,
+			additionalLatencyExpected: 0,
+		},
+		{
+			name:       "request verb is create, watches registered",
+			requestURI: "http://server/apis/foo.bar/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                29,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        3,
+			additionalLatencyExpected: 5 * time.Millisecond,
+		},
+		{
+			name:       "request verb is create, watches registered, no additional latency",
+			requestURI: "http://server/apis/foo.bar/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                5,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        0,
+			additionalLatencyExpected: 0,
+		},
+		{
+			name:       "request verb is create, watches registered, maximum is exceeded",
+			requestURI: "http://server/apis/foo.bar/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                199,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        20,
+			additionalLatencyExpected: 5 * time.Millisecond,
+		},
+		{
+			name:       "request verb is update, no watches",
+			requestURI: "http://server/apis/foo.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "update",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        0,
+			additionalLatencyExpected: 0,
+		},
+		{
+			name:       "request verb is update, watches registered",
+			requestURI: "http://server/apis/foor.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "update",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                29,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        3,
+			additionalLatencyExpected: 5 * time.Millisecond,
+		},
+		{
+			name:       "request verb is patch, no watches",
+			requestURI: "http://server/apis/foo.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "patch",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        0,
+			additionalLatencyExpected: 0,
+		},
+		{
+			name:       "request verb is patch, watches registered",
+			requestURI: "http://server/apis/foo.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "patch",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                29,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        3,
+			additionalLatencyExpected: 5 * time.Millisecond,
+		},
+		{
+			name:       "request verb is delete, no watches",
+			requestURI: "http://server/apis/foo.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "delete",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        0,
+			additionalLatencyExpected: 0,
+		},
+		{
+			name:       "request verb is delete, watches registered",
+			requestURI: "http://server/apis/foo.bar/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "delete",
+				APIGroup: "foo.bar",
+				Resource: "foos",
+			},
+			watchCount:                29,
+			initialSeatsExpected:      1,
+			finalSeatsExpected:        3,
+			additionalLatencyExpected: 5 * time.Millisecond,
+		},
 	}
 
 	for _, test := range tests {
@@ -259,7 +389,10 @@ func TestWorkEstimator(t *testing.T) {
 			countsFn := func(key string) (int64, error) {
 				return counts[key], test.countErr
 			}
-			estimator := NewWorkEstimator(countsFn)
+			watchCountsFn := func(_ *apirequest.RequestInfo) int {
+				return test.watchCount
+			}
+			estimator := NewWorkEstimator(countsFn, watchCountsFn)
 
 			req, err := http.NewRequest("GET", test.requestURI, nil)
 			if err != nil {
@@ -272,7 +405,13 @@ func TestWorkEstimator(t *testing.T) {
 
 			workestimateGot := estimator.EstimateWork(req)
 			if test.initialSeatsExpected != workestimateGot.InitialSeats {
-				t.Errorf("Expected work estimate to match: %d seats, but got: %d seats", test.initialSeatsExpected, workestimateGot.InitialSeats)
+				t.Errorf("Expected work estimate to match: %d initial seats, but got: %d", test.initialSeatsExpected, workestimateGot.InitialSeats)
+			}
+			if test.finalSeatsExpected != workestimateGot.FinalSeats {
+				t.Errorf("Expected work estimate to match: %d final seats, but got: %d", test.finalSeatsExpected, workestimateGot.FinalSeats)
+			}
+			if test.additionalLatencyExpected != workestimateGot.AdditionalLatency {
+				t.Errorf("Expected work estimate to match additional latency: %v, but got: %v", test.additionalLatencyExpected, workestimateGot.AdditionalLatency)
 			}
 		})
 	}

wojtek-t/kubernetes

staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -222,9 +222,6 @@ type Config struct {
 	// If not specify any in flags, then genericapiserver will only enable defaultAPIResourceConfig.
 	MergedResourceConfig *serverstore.ResourceConfig
 
-	// RequestWidthEstimator is used to estimate the "width" of the incoming request(s).
-	RequestWidthEstimator flowcontrolrequest.WidthEstimatorFunc
-
 	// lifecycleSignals provides access to the various signals
 	// that happen during lifecycle of the apiserver.
 	// it's intentionally marked private as it should never be overridden.
@@ -359,7 +356,6 @@ func NewConfig(codecs serializer.CodecFactory) *Config {
 		// Default to treating watch as a long-running operation
 		// Generic API servers have no inherent long-running subresources
 		LongRunningFunc:           genericfilters.BasicLongRunningRequestCheck(sets.NewString("watch"), sets.NewString()),
-		RequestWidthEstimator:     flowcontrolrequest.NewWidthEstimator(storageObjectCountTracker.Get),
 		lifecycleSignals:          newLifecycleSignals(),
 		StorageObjectCountTracker: storageObjectCountTracker,
 
@@ -753,8 +749,9 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	handler = filterlatency.TrackStarted(handler, "authorization")
 
 	if c.FlowControl != nil {
+		requestWidthEstimator := flowcontrolrequest.NewWidthEstimator(c.StorageObjectCountTracker.Get, c.FlowControl.GetInterestedWatchCount)
 		handler = filterlatency.TrackCompleted(handler)
-		handler = genericfilters.WithPriorityAndFairness(handler, c.LongRunningFunc, c.FlowControl, c.RequestWidthEstimator)
+		handler = genericfilters.WithPriorityAndFairness(handler, c.LongRunningFunc, c.FlowControl, requestWidthEstimator)
 		handler = filterlatency.TrackStarted(handler, "priorityandfairness")
 	} else {
 		handler = genericfilters.WithMaxInFlightLimit(handler, c.MaxRequestsInFlight, c.MaxMutatingRequestsInFlight, c.LongRunningFunc)

staging/src/k8s.io/apiserver/pkg/util/flowcontrol/request/width.go
@@ -45,11 +45,18 @@ func (f objectCountGetterFunc) Get(key string) int64 {
 	return f(key)
 }
 
+type watchCountGetterFunc func(*apirequest.RequestInfo) int
+
+func (f watchCountGetterFunc) Get(requestInfo *apirequest.RequestInfo) int {
+	return f(requestInfo)
+}
+
 // NewWidthEstimator calculates the width of the given request, if no WidthEstimatorFunc
 // matches the given request then the default width with '1' Seats is returned.
-func NewWidthEstimator(countFn objectCountGetterFunc) WidthEstimatorFunc {
+func NewWidthEstimator(objectCountFn objectCountGetterFunc, watchCountFn watchCountGetterFunc) WidthEstimatorFunc {
 	estimator := &widthEstimator{
-		listWidthEstimator: newListWidthEstimator(countFn),
+		listWidthEstimator:     newListWidthEstimator(objectCountFn),
+		mutatingWidthEstimator: newMutatingWidthEstimator(watchCountFn),
 	}
 	return estimator.estimate
 }
@@ -66,6 +73,8 @@ func (e WidthEstimatorFunc) EstimateWidth(r *http.Request) Width {
 type widthEstimator struct {
 	// listWidthEstimator calculates the width of list request(s)
 	listWidthEstimator WidthEstimatorFunc
+	// mutatingWidthEstimator calculates the width of mutating request(s)
+	mutatingWidthEstimator WidthEstimatorFunc
 }
 
 func (e *widthEstimator) estimate(r *http.Request) Width {
@@ -79,6 +88,8 @@ func (e *widthEstimator) estimate(r *http.Request) Width {
 	switch requestInfo.Verb {
 	case "list":
 		return e.listWidthEstimator.EstimateWidth(r)
+	case "create", "update", "patch", "delete":
+		return e.mutatingWidthEstimator.EstimateWidth(r)
 	}
 
 	return Width{Seats: minimumSeats}

staging/src/k8s.io/apiserver/pkg/util/flowcontrol/request/width_test.go
@@ -26,19 +26,23 @@ import (
 func TestWidthEstimator(t *testing.T) {
 	tests := []struct {
 		name          string
+		verb          string
 		requestURI    string
 		requestInfo   *apirequest.RequestInfo
 		counts        map[string]int64
+		watchCount    int
 		seatsExpected uint
 	}{
 		{
 			name:          "request has no RequestInfo",
+			verb:          "GET",
 			requestURI:    "http://server/apis/v1/foos/",
 			requestInfo:   nil,
 			seatsExpected: 10,
 		},
 		{
 			name:       "request verb is not list",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/",
 			requestInfo: &apirequest.RequestInfo{
 				Verb: "get",
@@ -47,6 +51,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, resource version not set",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?limit=499",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -60,6 +65,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, continuation is set",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?continue=token&limit=499&resourceVersion=1",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -73,6 +79,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, has limit",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?limit=499&resourceVersion=1",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -86,6 +93,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, resource version is zero",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?resourceVersion=0",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -99,6 +107,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, no query parameters, count known",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -112,6 +121,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, no query parameters, count not known",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -122,6 +132,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, resource version match is Exact",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?resourceVersion=foo&resourceVersionMatch=Exact&limit=499",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -135,6 +146,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, resource version match is NotOlderThan, limit not specified",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?resourceVersion=foo&resourceVersionMatch=NotOlderThan",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -148,6 +160,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, maximum is capped",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?resourceVersion=foo",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -161,6 +174,7 @@ func TestWidthEstimator(t *testing.T) {
 		},
 		{
 			name:       "request verb is list, list from cache, count not known",
+			verb:       "GET",
 			requestURI: "http://server/apis/v1/foos/1?resourceVersion=0&limit=799",
 			requestInfo: &apirequest.RequestInfo{
 				Verb:     "list",
@@ -168,6 +182,110 @@ func TestWidthEstimator(t *testing.T) {
 				Resource: "resource",
 			},
 			seatsExpected: 10,
+		},
+		{
+			name:       "request verb is create, no watches",
+			verb:       "POST",
+			requestURI: "http://server/apis/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			seatsExpected: 1,
+		},
+		{
+			name:       "request verb is create, watches registered",
+			verb:       "POST",
+			requestURI: "http://server/apis/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			watchCount:    299,
+			seatsExpected: 3,
+		},
+		{
+			name:       "request verb is create, watches registered, maximum is capped",
+			verb:       "POST",
+			requestURI: "http://server/apis/v1/foos",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "create",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			watchCount:    1999,
+			seatsExpected: 10,
+		},
+		{
+			name:       "request verb is update, no watches",
+			verb:       "PUT",
+			requestURI: "http://server/apis/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "update",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			seatsExpected: 1,
+		},
+		{
+			name:       "request verb is update, watches registered",
+			verb:       "POST",
+			requestURI: "http://server/apis/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "update",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			watchCount:    299,
+			seatsExpected: 3,
+		},
+		{
+			name:       "request verb is patch, no watches",
+			verb:       "PATCH",
+			requestURI: "http://server/apis/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "patch",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			seatsExpected: 1,
+		},
+		{
+			name:       "request verb is patch, watches registered",
+			verb:       "POST",
+			requestURI: "http://server/apis/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "patch",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			watchCount:    299,
+			seatsExpected: 3,
+		},
+		{
+			name:       "request verb is delete, no watches",
+			verb:       "DELETE",
+			requestURI: "http://server/apis/v1/foos/myfot",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "delete",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			seatsExpected: 1,
+		},
+		{
+			name:       "request verb is delete, watches registered",
+			verb:       "DELETE",
+			requestURI: "http://server/apis/v1/foos/myfoo",
+			requestInfo: &apirequest.RequestInfo{
+				Verb:     "delete",
+				APIGroup: "foo.bar",
+				Resource: "resource",
+			},
+			watchCount:    299,
+			seatsExpected: 3,
 		},
 	}
 
@@ -180,9 +298,12 @@ func TestWidthEstimator(t *testing.T) {
 			countsFn := func(key string) int64 {
 				return counts[key]
 			}
-			estimator := NewWidthEstimator(countsFn)
+			watchCountsFn := func(_ *apirequest.RequestInfo) int {
+				return test.watchCount
+			}
+			estimator := NewWidthEstimator(countsFn, watchCountsFn)
 
-			req, err := http.NewRequest("GET", test.requestURI, nil)
+			req, err := http.NewRequest(test.verb, test.requestURI, nil)
 			if err != nil {
 				t.Fatalf("Failed to create new HTTP request - %v", err)
 			}